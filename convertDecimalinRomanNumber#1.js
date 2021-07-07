17bf0a9642542c508f9234b61c75c85e3abb2202
ChrNitzsche <86894137+ChrNitzsche@users.noreply.github.com>
2021-07-07 16:08:42 +0200

    convertDecimalinRomanNumber

--- convertDecimalinRomanNumber#1.js
+++ convertDecimalinRomanNumber#1.js
@@ -1,46 +1,25 @@
 const arrRomanNumbers = ["I", "V", "X", "L", "C", "D", "M", "ↁ", "ↂ", "ↇ", "" /*! Undefined */ ];
 
 function transformDigit(digit) {
-    const V = -1; // predecessor of position "(var)intRomPos"
+    const V =-1; // predecessor of position "(var)intRomPos"
     const S = 0; // intRomPos = position in Array arrRomanNumbers
     const N = 1; // successor of position "(var)intRomPos"
 
     switch (digit) {
-        case "1":
-            return [V];
-            break;
-        case "2":
-            return [V, V];
-            break;
-        case "3":
-            return [V, V, V];
-            break;
-        case "4":
-            return [V, S];
-            break;
-        case "5":
-            return [S];
-            break;
-        case "6":
-            return [S, V];
-            break;
-        case "7":
-            return [S, V, V];
-            break;
-        case "8":
-            return [S, V, V, V];
-            break;
-        case "9":
-            return [V, N];
-            break;
-        case "0":
-            return [];
-            break;
+        case "1": return [V]; break;
+        case "2": return [V, V]; break;
+        case "3": return [V, V, V]; break;
+        case "4": return [V, S]; break;
+        case "5": return [S]; break;
+        case "6": return [S, V]; break;
+        case "7": return [S, V, V]; break;
+        case "8": return [S, V, V, V]; break;
+        case "9": return [V, N]; break;
+        case "0": return []; break;
     }
 }
 
-
-function position_arrRomanNumbers(strNum) { // arr ->  [ 5, 3, 1 ]
+function position_arrRomanNumbers(strNum) { // arrStellen ->  [ 5, 3, 1 ]
     let arrStellen = [];
     for (let i = strNum.length; i > 0; i--) {
         arrStellen.push(2 * i - 1);
@@ -64,7 +43,8 @@ function convertToRoman(num) {
     // T-H-Z-E bearbeitet wird -> Römische Zahl von hinten aufrollen
     intRomPos = position_arrRomanNumbers(strNum);
     for (let i = 0; i < strNum.length; i++) {
-        // arr = [-1,0,+1] -> daraus wird ein römischer Buchstabe abgegriffen.
+        // arr = [-1,0,+1] or [1,-1,-1], ... -> daraus wird mithilfe von inRomPos der korrekte römische Buchstabe abgegriffen. 
+        // intRomPos = 1 und arr = [1,-1,-1,-1] -> [X,I,I,I], wobei der Buchstabe "V" in arr "0" wäre;
         arr = transformDigit(strNum[i]);
         // arr umwandeln
         for (let j = 0; j < arr.length; j++) {
